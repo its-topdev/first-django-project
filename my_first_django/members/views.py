bda53789b6a9cc5b0c1251647bc60e9f65093047
Neil Adams <neil.zhao.adams@gmail.com>
2023-11-01 01:17:55 -0700

    initial commit

---
+++ my_first_django/members/views.py
@@ -0,0 +1,21 @@
+from django.shortcuts import render
+
+# Create your views here.
+from django.shortcuts import render
+from django.http import HttpResponse
+from django.template import loader
+from .models import Member
+
+
+def members(request):
+    mymembers = Member.objects.all().values()
+    template = loader.get_template("all_members.html")
+    context = {"mymembers": mymembers}
+    return HttpResponse(template.render(context, request))
+
+
+def details(request, id):
+    mymember = Member.objects.get(id=id)
+    template = loader.get_template("details.html")
+    context = {"mymember": mymember}
+    return HttpResponse(template.render(context, request))
