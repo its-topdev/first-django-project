54cb92047fc10c80d2b91f373f08483559703be7
Neil Adams <neil.zhao.adams@gmail.com>
2023-11-02 05:04:21 -0700

    static files added

--- my_first_django/members/views.py
+++ my_first_django/members/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 
 # Create your views here.
-from django.shortcuts import render
+
 from django.http import HttpResponse
 from django.template import loader
 from .models import Member
@@ -18,4 +18,16 @@ def details(request, id):
     mymember = Member.objects.get(id=id)
     template = loader.get_template("details.html")
     context = {"mymember": mymember}
+    return HttpResponse(template.render(context, request))
+
+
+def main(request):
+    template = loader.get_template("main.html")
+    return HttpResponse(template.render())
+
+
+def testing(request):
+    my_data = Member.objects.all()
+    template = loader.get_template("template.html")
+    context = {"mymembers": my_data}
     return HttpResponse(template.render(context, request))
--- my_first_django/my_first_django/settings.py
+++ my_first_django/my_first_django/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-y8fo1o7amt*j7(*_hx3ufc-)9n&k2jc53m+c66%f29%1*s(p!x"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
@@ -48,6 +48,7 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
+    "whitenoise.middleware.WhiteNoiseMiddleware",
 ]
 
 ROOT_URLCONF = "my_first_django.urls"
@@ -115,6 +116,8 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
+
+STATIC_ROOT = BASE_DIR / "productionfiles"
 
 STATIC_URL = "static/"
 
